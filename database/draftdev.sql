df4ea0a6a52467080b6b01c34dcc4c3add4f8401
Web expert <ellenabarasova@gmail.com>
2021-05-28 10:24:35 +0300

    sql upload

--- database/draftdev.sql
+++ database/draftdev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2021 at 10:51 AM
+-- Generation Time: May 28, 2021 at 09:22 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `divisions`
+--
+
+CREATE TABLE `divisions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `leagueId` bigint(20) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `divisions`
+--
+
+INSERT INTO `divisions` (`id`, `name`, `leagueId`, `created_at`, `updated_at`) VALUES
+(1, 'Division 1', NULL, '2021-05-25 12:01:15', '2021-05-25 12:01:15');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `failed_jobs`
 --
 
@@ -36,6 +57,55 @@ CREATE TABLE `failed_jobs` (
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leagues`
+--
+
+CREATE TABLE `leagues` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `playoff_teamname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `playoff_tiebreaker` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `teamId` bigint(20) DEFAULT NULL,
+  `sportId` bigint(20) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `userId` bigint(20) DEFAULT NULL COMMENT 'id of creator'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `leagues`
+--
+
+INSERT INTO `leagues` (`id`, `name`, `playoff_teamname`, `playoff_tiebreaker`, `teamId`, `sportId`, `created_at`, `updated_at`, `userId`) VALUES
+(1, 'e-league1', NULL, NULL, 1, NULL, '2021-05-27 10:37:44', '2021-05-27 10:53:53', 1),
+(2, 'e-league2', NULL, NULL, 2, NULL, '2021-05-27 11:18:39', '2021-05-27 11:18:42', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matchups`
+--
+
+CREATE TABLE `matchups` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `matchupDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `leagueId` bigint(20) NOT NULL,
+  `winningTeamId` bigint(20) NOT NULL,
+  `losingTeamId` bigint(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `matchups`
+--
+
+INSERT INTO `matchups` (`id`, `matchupDate`, `leagueId`, `winningTeamId`, `losingTeamId`, `created_at`, `updated_at`) VALUES
+(1, '2021-05-25 15:30:39', 1, 1, 2, '2021-05-25 15:30:18', '2021-05-25 15:30:21');
 
 -- --------------------------------------------------------
 
@@ -56,7 +126,12 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2021_05_21_025912_create_sports_table', 2),
+(5, '2021_05_21_030159_create_divisions_table', 2),
+(6, '2021_05_21_030216_create_teams_table', 2),
+(7, '2021_05_21_030235_create_leagues_table', 2),
+(8, '2021_05_21_030248_create_matchups_table', 2);
 
 -- --------------------------------------------------------
 
@@ -69,6 +144,55 @@ CREATE TABLE `password_resets` (
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sports`
+--
+
+CREATE TABLE `sports` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sports`
+--
+
+INSERT INTO `sports` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Football', '2021-05-25 11:50:42', '2021-05-25 11:53:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teams`
+--
+
+CREATE TABLE `teams` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userId` bigint(20) DEFAULT NULL,
+  `divisionId` bigint(20) DEFAULT NULL,
+  `WLT` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Points` double(8,2) DEFAULT NULL,
+  `Streak` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Waiver` bigint(20) DEFAULT NULL,
+  `Moves` bigint(20) DEFAULT NULL,
+  `leagueId` bigint(20) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `teams`
+--
+
+INSERT INTO `teams` (`id`, `name`, `userId`, `divisionId`, `WLT`, `Points`, `Streak`, `Waiver`, `Moves`, `leagueId`, `created_at`, `updated_at`) VALUES
+(1, 'Team1', NULL, 1, '2', 100.00, '5', 1, 5, 1, '2021-05-25 12:20:27', '2021-05-25 12:20:27'),
+(2, 'Team2', NULL, 1, '3', 4.00, '3', 3, 5, 1, '2021-05-25 12:31:03', '2021-05-25 12:31:03');
 
 -- --------------------------------------------------------
 
@@ -90,7 +214,6 @@ CREATE TABLE `users` (
   `country` varchar(50) DEFAULT NULL,
   `city` varchar(50) DEFAULT NULL,
   `zip_code` varchar(50) DEFAULT NULL,
-  `profile_image` varchar(255) DEFAULT NULL,
   `company_name` varchar(191) DEFAULT NULL,
   `phone` varchar(50) DEFAULT NULL,
   `sessionid` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -98,19 +221,27 @@ CREATE TABLE `users` (
   `user_agent` varchar(300) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `remember_token` varchar(100) DEFAULT NULL
+  `remember_token` varchar(100) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `role`, `first_name`, `last_name`, `email`, `email_verified_at`, `password`, `age`, `birthday`, `gender`, `country`, `city`, `zip_code`, `profile_image`, `company_name`, `phone`, `sessionid`, `ip`, `user_agent`, `created_at`, `updated_at`, `remember_token`) VALUES
-(1, 'user', 'ermal', 'joko', 'ermal.up@gmail.com', '2021-05-19 06:06:33', '$2y$10$UONFOQrVGdwyADpm5jOjm.juW9QjGPVBdereCzb2QFI0opcImT6g6', 28, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2021-05-19 03:06:33', '2021-05-19 03:06:33', NULL);
+INSERT INTO `users` (`id`, `role`, `first_name`, `last_name`, `email`, `email_verified_at`, `password`, `age`, `birthday`, `gender`, `country`, `city`, `zip_code`, `company_name`, `phone`, `sessionid`, `ip`, `user_agent`, `created_at`, `updated_at`, `remember_token`, `photo`) VALUES
+(1, 'user', 'ermal', 'joko', 'ermal.up@gmail.com', '2021-05-19 06:06:33', '$2y$10$UONFOQrVGdwyADpm5jOjm.juW9QjGPVBdereCzb2QFI0opcImT6g6', 28, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2021-05-19 03:06:33', '2021-05-19 03:06:33', 'nw0hBCA1lttEDkA1VfhbY3A47wwrF9z7Kohxyn1LRpS7AjOHCUiBPQvF4pr1', NULL),
+(2, 'admin', 'admin', '', 'admin@gmail.com', '2021-05-21 08:01:02', '$2y$10$UONFOQrVGdwyADpm5jOjm.juW9QjGPVBdereCzb2QFI0opcImT6g6', 30, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2021-05-21 08:01:02', '2021-05-21 08:01:02', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `divisions`
+--
+ALTER TABLE `divisions`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -118,6 +249,18 @@ INSERT INTO `users` (`id`, `role`, `first_name`, `last_name`, `email`, `email_ve
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `leagues`
+--
+ALTER TABLE `leagues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `matchups`
+--
+ALTER TABLE `matchups`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -132,6 +275,19 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `sports`
+--
+ALTER TABLE `sports`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sports_name_unique` (`name`);
+
+--
+-- Indexes for table `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -143,76 +299,54 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `divisions`
+--
+ALTER TABLE `divisions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `leagues`
+--
+ALTER TABLE `leagues`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `matchups`
+--
+ALTER TABLE `matchups`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `sports`
+--
+ALTER TABLE `sports`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-DROP TABLE IF EXISTS sports;
-CREATE TABLE sports (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS divisions;
-CREATE TABLE divisions (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  leagueId BIGINT NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS teams;
-CREATE TABLE teams (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  userId BIGINT,
-  divisionId BIGINT,
-  WLT VARCHAR(20) NOT NULL,
-  Points float(11) NOT NULL,
-  Streak VARCHAR(20) NOT NULL,
-  Waiver BIGINT,
-  Moves BIGINT,
-  leagueId BIGINT NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS leagues;
-CREATE TABLE leagues (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  playoff_teamname VARCHAR(255) NOT NULL,
-  playoff_tiebreaker VARCHAR(255) NOT NULL,
-  teams_num BIGINT NOT NULL,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  sportId BIGINT NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS matchups;
-CREATE TABLE matchups (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  matchupDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  leagueId BIGINT NOT NULL,
-  winningTeamId BIGINT,
-  losingTeamId BIGINT
- 
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
